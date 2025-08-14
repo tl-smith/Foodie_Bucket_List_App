5736805c2c822aed4da6eb2fecea12fdc7ac0508
Winona Murphy <wviolinm@gmail.com>
2025-08-13 23:01:47 -0400

    Current progress with routes
    Create GET route for bucket lists, but struggling
    to create POST route for bucket lists. Will edit
    Prisma schema before continuing.
    
    Implements-Requirement: #27

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import verifyToken from "./middleware/auth.js";
+import bucketListRouter from "./routes/bucketList.js"
 
 const app = express();
 const PORT = 8080;
@@ -8,7 +9,13 @@ const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 
+app.use("/bucket-lists", bucketListRouter);
+
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.get("/user-profile", async (req, res) => {
+    res.send("GET /user-profile In progress");
+})
 
 app.listen(PORT, () => {
    console.log(`Server running on http://localhost:${PORT}`); 
