63124e7377c5ba559978d6299a2d0627396cc8d7
Winona Murphy <wviolinm@gmail.com>
2025-08-18 19:36:51 -0400

    Remove unneeded pages
    Remove routes, files, and import statements for
    Login, Signup, BucketList, and ContactUs page.
    Also remove App.* files.
    
    Implements-Requirement: #70

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,32 +3,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx';
-import Contact from './pages/ContactUs.jsx'
-import Login from './pages/Login.jsx'
-import SignUp from './pages/SignUp.jsx'
-import Dashboard from './pages/Dashboard.jsx'
-import BucketList from './pages/BucketList.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 import './index.css'
 
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(  
-
-    <BrowserRouter>
-      <Routes>
-
-        <Route path="/" element={< Home />} />
-        <Route path="/about" element={< About />} />
-        <Route path="/contact" element={< Contact />} />
-        <Route path="/login" element={< Login />} />
-        <Route path="/signup" element={< SignUp />} />
-        <Route path="/dashboard" element={< Dashboard />} />
-        <Route path="/create-bucket-list" element={< BucketList />} />
-
-      </Routes>
-    </BrowserRouter>
-  
-
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={< Home />} />
+      <Route path="/about" element={< About />} />
+      <Route path="/dashboard" element={< Dashboard />} />
+    </Routes>
+  </BrowserRouter>
 );
 
--- frontend/src/pages/ContactUs.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Contact() {
-  return (
-    <div>
-      <h1>Contact us Page!</h1>
-    </div>
-  );
-}
-
