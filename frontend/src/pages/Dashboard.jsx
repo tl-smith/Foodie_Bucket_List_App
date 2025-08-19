9bf18c6b87ba0d7d7f314432fcb22b162240723a
Winona Murphy <wviolinm@gmail.com>
2025-08-18 20:41:09 -0400

    Remove Headers and Footers from Home and Dashboard
    Removed the custom Header and Footer components
    made for the Home and Dashboard pages to make the
    look and functionality more consistent
    
    Implements-Requirement: #65

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,9 @@
-
-import DashboardHeader from "../components/DashboardHeader"
 import DashboardHero from "../components/DashboardHero"
 import BucketListGrid from "../components/BucketListGrid"
 
 export default function Dashboard() {
   return (
     <div className="min-h-screen bg-stone-50">
-      <DashboardHeader />
       <DashboardHero />
       <div className="py-16">
         <BucketListGrid />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,11 @@
-import Header from "../components/header"
 import Hero from "../components/Hero"
 import HowItWorks from "../components/HowItWorks"
-import Footer from "../components/Footer"
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Header />
       <Hero />
       <HowItWorks />
-      <Footer />
     </div>
   )
 }
