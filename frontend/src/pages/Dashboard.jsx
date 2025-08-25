703eb8415db03db373b0d80714e65449815fa5d4
joselevelsup <joselevelsup@gmail.com>
2025-08-24 20:52:50 -0400

    feat: changed design to resemble designs and what was in ticket

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,9 +3,14 @@ import BucketListGrid from "../components/BucketListGrid"
 
 export default function Dashboard() {
   return (
-    <div className="min-h-screen bg-stone-50">
+    <div className="min-h-screen">
       <DashboardHero />
-      <div className="py-16">
+      <div className="text-center my-10">
+        <button className="bg-teal-500 hover:bg-teal-600 text-white px-8 py-3 rounded-lg text-lg">
+          Create Bucketlist
+        </button>
+      </div>
+      <div className="py-12">
         <BucketListGrid />
       </div>
     </div>
